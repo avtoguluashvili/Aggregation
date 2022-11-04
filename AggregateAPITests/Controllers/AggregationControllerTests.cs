a67ea2813e6be3b2aea327d39fca5d048abe0c39
avto.guluashvili <avto.guluashvili@gmail.com>
2022-11-04 15:45:41 +0400

    initial

---
+++ AggregateAPITests/Controllers/AggregationControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AggregateAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class AggregationControllerTests
+    {
+        [TestMethod()]
+        public void SaveCsvDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
